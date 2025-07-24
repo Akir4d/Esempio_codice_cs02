da0e9dacbcfb465af1e8c13b2122dc444605f73a
Paolo Rampino <liadserv@gmail.com>
2025-07-24 13:47:58 -0400

    first commit

---
+++ base.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+Programma in C
+piu' linee
+*/
+// Commento in linea
+
+int main(){
+    printf("**********************\n");
+    printf("*****Hello World!*****\n");
+    printf("**********************\n");
+}
+
