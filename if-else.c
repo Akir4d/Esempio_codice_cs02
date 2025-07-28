bc3f07ee744c2170cf4cb84feb6c3cb131f0b376
Paolo R <paolo@io.io>
2025-07-28 13:24:06 -0400

    up

---
+++ if-else.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int numeratore;
+    int denominatore;
+
+    printf("\nDammi in numeratore valore: ");
+    scanf("%d", &numeratore);
+    //printf("zona di memoria 0x%x contiene %d", &numeratore, numeratore);
+    printf("\nDammi il denominatore valore: ");
+    scanf("%d", &denominatore);
+
+
+    return 0;
+}
