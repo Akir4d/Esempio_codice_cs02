8f3f70cc8fca819813e8e5c4e7077c614bf9576e
Paolo Rampino <liadserv@gmail.com>
2025-08-04 15:34:50 -0400

    UP

---
+++ scansione_porte.py
@@ -0,0 +1,32 @@
+import socket as so
+
+# chiediamo come scansire le porte di un trget
+target = input("Inserisci l'indirizzo da scansire: ")
+prange = input("Inserisci il range delle porte (es 5-200): ")
+
+# uso split per ricavare un array con la porta superiore e inferiore
+rrange = prange.split("-")
+lowport = int(rrange[0]) # questo c'e' sempre anche se l'utente dimentica -
+highport = 65535 # qui mettiamo la porta massima
+if rrange[1]:
+    highport = int(rrange[1])
+print(f"Scansisco il target {target} dalla porta {lowport} alla porta {highport}")
+porte_chiuse = []
+#faccio partire un ciclo for con il range porte da scansire
+for port in range(lowport, highport+1):
+    # avviamo una connessione tcp ipv4
+    s = so.socket(so.AF_INET, so.SOCK_STREAM)
+    # testiamo il 3-way handshake
+    status = s.connect_ex((target, port))
+    
+    if(status == 0):
+        # se 0 la connessione e' avvenuta, quindi siamo contenti
+        print(f"*** Porta {port} - Aperta ***")
+    else:
+        porte_chiuse.append(port)
+        #print(f"Porta {port} - chiusa")
+    s.close()
+
+domanda = input("vuoi vedere le porte chiuse? ")
+if domanda.upper().startswith("S"):
+        print(porte_chiuse)
