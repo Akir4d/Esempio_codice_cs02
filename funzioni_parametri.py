16c9eff3139c42dbd3f84dd470912496024a9478
Paolo Rampino <liadserv@gmail.com>
2025-08-04 13:34:11 -0400

    UP

--- funzioni_parametri.py
+++ funzioni_parametri.py
@@ -7,6 +7,6 @@ def sottrazione_infallibile(x=1,y=1):
 print("Sottrazione:", sottrazione(5, 4))
 print("Sottrazione:", sottrazione(y=4, x=5))
 print("Sottrazione Infallibile:", sottrazione_infallibile(5))
-print("Sottrazione Infallibile:", sottrazione_infallibile(5, 2))
+print("Sottrazione Infallibile:", sottrazione_infallibile(5, 2), sep="")
 print("Sottrazione Infallibile:", sottrazione_infallibile(9,y=5))
 print("Sottrazione Infallibile:", sottrazione_infallibile(y=5))
